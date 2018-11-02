8021f76939421dc69f6ba68e47b09ca62edae532
Jay Coskey <jay.coskey@gmail.com>
2018-11-02 16:18:35 -0700

    Add methods to config files and config entry to their constituent rows

--- Python/util.py
+++ Python/util.py
@@ -13,8 +13,19 @@ class Util:
         are_rows_same_length = all(map(lambda row: len(row) == row_count, rows))
         if not are_rows_same_length:
             return False
-        is_board_square = row_count = len(rows[0])
+        is_board_square = row_count == len(rows[0])
         return is_board_square
+
+    @staticmethod
+    def get_rows_from_config(cfg):
+        return [line for line in cfg.split('\n') if len(line) > 0]
+
+    @staticmethod
+    def get_rows_from_filename(filename):
+        with open(filename, 'r') as f:
+            result = [line.strip() for line in f.readlines()]
+            print(f'# of rows in result={len(result)}')
+            return [line.strip() for line in f.readlines()]
 
     @staticmethod
     def reversed_dict(d):
