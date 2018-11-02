1d62aa571357816e823f94566a9138d2662b1cf5
Jay Coskey <jay.coskey@gmail.com>
2018-11-01 23:50:32 -0700

    Separate concepts of Move and Turn. A Move is letters placed and words formed. A Turn could be a Move, a swap, or a resignation.

--- Python/move.py
+++ Python/move.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python
 
-from enum import Enum
-
-
-class MoveType(Enum):
-    NO_MOVE = 0
-    PLACE = 1
-    SWAP = 2
-    PASS = 3
-    RESIGN = 4
 
 class Move:
     def __init__(self):
-        # TODO: self.move_num: int
-        # TODO: self.player_id: int
-        # TODO: self.move_type: MoveType
-        # TODO: self.played_letters: List[PlayedLetter]
-        # TODO: self.played_words: List[PlayedWord]
-        # TODO: self.letters_discarded: str
-        # TODO: self.letters_drawn: str
+        # TODO: self.played_letters: List[PlacedLetter]
+        # TODO: self.primary_word: PlacedWord
+        # TODO: self.secondary_words: List[PlacedWord]
         pass
 
 
-class PlayedLetter:
-    # TODO: self.player_id: int
-    # TODO: self.char: str
-    # TODO: self.square: Square
-    pass
+# Note: We'll use uppercase to represent the character used for blank tiles, and lowercase for all others.
+class PlacedLetter:
+    def __init__(self):
+        # TODO: self.player_id: int
+        # TODO: self.char: str
+        # TODO: self.square: Square
+        pass
 
 
-class PlayedWord:
-    # TODO: self.word: str
-    # TODO: self.player_id: int
-    # TODO: self.square_begin: str
-    # TODO: self.square_end: str
-    pass
+class PlacedWord:
+    def __init__(self):
+        # TODO: self.word: str
+        # TODO: self.player_id: int
+        # TODO: self.square_begin: str
+        # TODO: self.square_end: str
+        pass
---
+++ Python/turn.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from enum import Enum
+
+
+class TurnType(Enum):
+    NO_TURN = 0
+    MOVE = 1
+    SWAP = 2
+    PASS = 3
+    RESIGN = 4
+
+
+class Turn:
+    def __init__(self):
+        # TODO: self.move_num: int
+        # TODO: self.player_id: int
+        # TODO: self.move_type: MoveType
+        # TODO: self.move: Move
+        # TODO: self.letters_discarded: str
+        # TODO: self.letters_drawn: str
+        # TODO: self.points: str
+        pass
