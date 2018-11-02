46c44744a48ae63e919f093b624250bb50398f92
Jay Coskey <jay.coskey@gmail.com>
2018-11-01 23:52:39 -0700

    Add argument parsing to the main script

--- Python/word_square_game.py
+++ Python/word_square_game.py
@@ -1,5 +1,6 @@
 #!//usr/bin/env python
 
+import argparse
 import string
 import sys
 import yaml
@@ -7,12 +8,21 @@ import yaml
 from board import Board, BoardLayout
 from game import Game
 from gtree import GTree
-from move import Move, PlayedLetter, PlayedWord
+from move import Move, PlacedLetter, PlacedWord
 from player import Player
 from util import Util
 
 
 def main():
+    for line in layout_rows:
+        print(line)
+
+
+if __name__ == '__main__':
+    prog_description = 'Toolbox for word square games. Details TBD'
+    parser = argparse.ArgumentParser(prog_description)
+    args = parser.parse_args()
+
     with open('config.yml', 'r') as config_stream:
         try:
             config = yaml.load(config_stream)
@@ -23,10 +33,4 @@ def main():
     layout_rows = layout.splitlines(False)
     assert(Util.do_rows_form_square(layout_rows))
 
-    for line in layout_rows:
-        print(line)
-
-
-if __name__ == '__main__':
-    # TODO: Parse arguments
     main()
