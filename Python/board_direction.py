4e83ce165c4c10b2cbe7671b18ebd6549c7cc10a
Jay Coskey <jay.coskey@gmail.com>
2018-11-14 20:50:52 -0800

    Swap UP & DOWN values

--- Python/board_direction.py
+++ Python/board_direction.py
@@ -8,8 +8,8 @@ class BoardDirection(Enum):
     NO_DIR = (0, 0)
     LEFT = (-1, 0)
     RIGHT = (1, 0)
-    UP = (0, 1)
-    DOWN = (0, -1)
+    UP = (0, -1)
+    DOWN = (0, 1)
 
     @staticmethod
     def is_forward(bdir):
